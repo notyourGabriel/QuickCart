3df43ac226259242bfab4c661f20fd95a5ac70dc
notyourGabriel <elbastian0310@gmail.com>
2025-05-12 21:46:21 +0700

    FirstCommit

---
+++ config/db.js
@@ -0,0 +1,31 @@
+import { Mongoose } from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false
+        }
+
+        cached.promise = Mongoose.connect(`${process.env.MONGODB_URI}/quickcart` ,opts).then(mongoose => {
+            return mongoose;
+        });
+
+    }
+
+    cached.conn = await cached.promise
+    return cached.conn;
+
+}
+
+export default connectDB;
