f5ec04c4e173824d07a9dcf7fd037dfc9b760652
notyourGabriel <elbastian0310@gmail.com>
2025-05-13 20:01:07 +0700

    Save3

--- models/Order.js
+++ models/Order.js
@@ -2,17 +2,17 @@ import mongoose from "mongoose";
 
 
 const orderSchema = new mongoose.Schema({
-    userId: {type: String, required: true, ref: "user"},
+    userId: {type: String, required: true, ref: "User"},
     items: [{
-        products: {type: String, required: true, ref: "products"},
+        products: {type: String, required: true, ref: "Products"},
         quantity: {type: Number, required: true}
     }],
     amount: {type: Number, required: true},
-    addres: {type: String, ref: "address", required: true},
+    address: {type: String, ref: "Address", required: true},
     status: {type: String, required: true, default: "Order Placed"},
     date : {type: Date, required: true},
 })
 
-const Order = mongoose.models.Order || mongoose.model("order", orderSchema)
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema)
 
 export default Order;
