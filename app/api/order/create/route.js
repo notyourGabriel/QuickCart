348c98c28fbf58409636d3f7aa1bfc342acd1546
notyourGabriel <elbastian0310@gmail.com>
2025-05-13 19:16:18 +0700

    Save1

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,7 +1,6 @@
 import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
-import User from "@/models/User";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth, User } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
