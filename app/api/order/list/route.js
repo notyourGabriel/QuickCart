c183828f67f135372efad3cb746aa59ede375ce4
notyourGabriel <elbastian0310@gmail.com>
2025-05-13 22:41:45 +0700

    test

--- app/api/order/list/route.js
+++ app/api/order/list/route.js
@@ -10,7 +10,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
     try {
         
-        const {userId} =getAuth(request)
+        const {userId} = getAuth(request)
 
         await connectDB()
 
