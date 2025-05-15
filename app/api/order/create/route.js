1cc15d481de8c9477aaf6713cce2df29f56380a3
notyourGabriel <elbastian0310@gmail.com>
2025-05-15 21:28:10 +0700

    SAVE

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,7 +1,7 @@
 import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
 import User from "@/models/User";
-import { getAuth} from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/dist/types/server";
 import { NextResponse } from "next/server";
 
 
@@ -10,38 +10,37 @@ export async function POST(request) {
     try {
         
         const {userId} = getAuth(request)
-        const {address, items} = await request.json();
-
+        const {address, items } = await request.json()
+        
         if (!address || items.length === 0) {
-            return NextResponse.json({success: false, message: "Invalid data"})
-        }
+            return NextResponse.json({success: false, message: "Invalid data"});
+        } 
 
-        // Calculate amount using amount
-        const amount = items.reduce(async (acc, item) => {
+        //calculate total amount using items
+        const amount = await items.reduce(async (acc, item) => {
             const product = await Product.findById(item.product);
-            return await acc + product.offerPrice * item.quantity;
-        },0)
+            return acc + product.offerPrice * item.quantity;
+        },0)    
 
         await inngest.send({
-            name: 'order/created',
+            name: 'order.created',
             data: {
                 userId,
                 address,
                 items,
                 amount: amount + Math.floor(amount * 0.02),
-                date: Date.now(),
+                date: Date.now()
             }
         })
 
         //clear user cart
         const user = await User.findById(userId)
-        user.cartItems = {}
+        user.cart = {}
         await user.save()
 
         return NextResponse.json({success: true, message: "Order Placed"})
 
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({success: false, message: error.message})
+        return NextResponse.json({success: false, message: error.message});
     }
 }
--- models/Order.js
+++ models/Order.js
@@ -2,17 +2,17 @@ import mongoose from "mongoose";
 
 
 const orderSchema = new mongoose.Schema({
-    userId: {type: String, required: true, ref: 'user'},
+    userId: {type: String, required: true},
     items: [{
-        products: {type: String, required: true, ref: 'products'},
+        product: {type: String, required: true, ref: 'product'},
         quantity: {type: Number, required: true}
     }],
     amount: {type: Number, required: true},
-    address: {type: String, ref: 'address', required: true},
-    status: {type: String, required: true, default: "Order Placed"},
-    date : {type: Number, required: true},
+    address: {type: String, ref: 'address, required: true'},
+    status: {type: String, required: true, default: 'Order Placed'},
+    date: { type: Number, required: true},
 })
 
-const Order = mongoose.models.Order || mongoose.model("order", orderSchema)
+const Order = mongoose.models.order || mongoose.model('order', orderSchema)
 
 export default Order;
